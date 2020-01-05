73d2c3489b705c8c73508e48de2766358c685a26
bingining <bingining@gmail.com>
2020-01-05 14:52:42 +0000

    upload files

---
+++ code/models/models.py
@@ -0,0 +1,244 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+import numpy as np
+
+from enterprise.signals import parameter
+from enterprise.signals import selections
+from enterprise.signals import signal_base
+from enterprise.signals import white_signals
+from enterprise.signals import gp_signals
+from enterprise.signals import deterministic_signals
+from enterprise.signals import utils
+
+from utils import get_noise_dict
+
+# import omegagw_funcs as OG
+# from omegagw_funcs import Ogw2
+
+H0 = 2.192711267238057e-18
+from numpy import pi
+from omegagw_funcs import STT, SGeneral, SInflation
+
+# range for log10@A
+logA_min, logA_max = -20, -10
+
+@signal_base.function
+def GWTT(f, log10_A, k):
+    df = np.diff(np.concatenate((np.array([0]), f[::2])))
+    return STT(f, 10**log10_A, k) * np.repeat(df, 2)
+
+@signal_base.function
+def GWGeneral(f, log10_A, k):
+    df = np.diff(np.concatenate((np.array([0]), f[::2])))
+    return SGeneral(f, 10**log10_A, k) * np.repeat(df, 2)
+
+@signal_base.function
+def GWInflation(f, log10_A):
+    df = np.diff(np.concatenate((np.array([0]), f[::2])))
+    return SInflation(f, 10**log10_A) * np.repeat(df, 2)
+
+
+#### Extra model componentents not part of base enterprise ####
+
+@signal_base.function
+def free_spectrum(f, log10_rho=None):
+    """
+    Free spectral model. PSD  amplitude at each frequency
+    is a free parameter. Model is parameterized by
+    S(f_i) = \rho_i^2 * T,
+    where \rho_i is the free parameter and T is the observation
+    length.
+    """
+    return np.repeat(10**(2*log10_rho), 2)
+
+
+#### Model component building blocks ####
+
+def white_noise_block(vary=False):
+    """
+    Returns the white noise block of the model:
+
+        1. EFAC per backend/receiver system
+        2. EQUAD per backend/receiver system
+        3. ECORR per backend/receiver system
+
+    :param vary:
+        If set to true we vary these parameters
+        with uniform priors. Otherwise they are set to constants
+        with values to be set later.
+    """
+
+    # define selection by observing backend
+    selection = selections.Selection(selections.by_backend)
+
+    # white noise parameters
+    if vary:
+        efac = parameter.Uniform(0.01, 10.0)
+        equad = parameter.Uniform(-8.5, -5)
+        ecorr = parameter.Uniform(-8.5, -5)
+    else:
+        efac = parameter.Constant()
+        equad = parameter.Constant()
+        ecorr = parameter.Constant()
+
+    # white noise signals
+    ef = white_signals.MeasurementNoise(efac=efac, selection=selection)
+    eq = white_signals.EquadNoise(log10_equad=equad, selection=selection)
+    ec = white_signals.EcorrKernelNoise(log10_ecorr=ecorr, selection=selection)
+
+    # combine signals
+    s = ef + eq + ec
+
+    return s
+
+
+def red_noise_block(prior='log-uniform', Tspan=None):
+    """
+    Returns red noise model:
+
+        1. Red noise modeled as a power-law with 30 sampling frequencies
+
+    :param prior:
+        Prior on log10_A. Default if "log-uniform". Use "uniform" for
+        upper limits.
+    :param Tspan:
+        Sets frequency sampling f_i = i / Tspan. Default will
+        use overall time span for indivicual pulsar.
+
+    """
+
+    # red noise parameters
+    if prior == 'uniform':
+        log10_A = parameter.LinearExp(-20, -11)
+    elif prior == 'log-uniform':
+        log10_A = parameter.Uniform(-20, -11)
+    else:
+        raise ValueError('Unknown prior for red noise amplitude!')
+
+    gamma = parameter.Uniform(0, 7)
+
+    # red noise signal
+    pl = utils.powerlaw(log10_A=log10_A, gamma=gamma)
+    rn = gp_signals.FourierBasisGP(pl, components=50, Tspan=Tspan)
+
+    return rn
+
+
+def common_red_noise_block(psd='inflation', 
+                           prior='log-uniform',
+                           Tspan=None, 
+                           orf="TT",
+                           name=None,
+                           logA_min=-30, 
+                           logA_max=-8):
+    """
+    Returns common red noise model:
+
+        1. Red noise modeled with user defined PSD with
+        30 sampling frequencies. Available PSDs are
+        ['powerlaw', 'turnover' 'spectrum']
+
+    :param psd:
+        PSD to use for common red noise signal. Available options
+        are ['powerlaw', 'turnover' 'spectrum']
+    :param prior:
+        Prior on log10_A. Default if "log-uniform". Use "uniform" for
+        upper limits.
+    :param Tspan:
+        Sets frequency sampling f_i = i / Tspan. Default will
+        use overall time span for indivicual pulsar.
+    :param orf:
+        String representing which overlap reduction function to use.
+        By default we do not use any spatial correlations. Permitted
+        values are ['hd', 'dipole', 'monopole'].
+    :param name: Name of common red process
+
+    """
+
+    orfs = {'TT': utils.TT_orf(), 'ST': utils.ST_orf(),
+            'VL': utils.VL_orf(), 'SL':utils.SL_orf()}
+
+    if name==None: 
+        name = orf
+        
+    Agw_name = 'log10_A_{}'.format(name)
+    if prior == 'uniform':  
+        log10_Agw = parameter.LinearExp(logA_min, logA_max)(Agw_name)
+    elif prior == 'log-uniform':
+        log10_Agw = parameter.Uniform(logA_min, logA_max)(Agw_name)        
+        
+    if psd == 'inflation':
+        cpl = GWInflation(log10_A=log10_Agw)
+
+    crn = gp_signals.FourierBasisCommonGP(cpl, orfs[orf], components=30, Tspan=Tspan, name=name)
+
+    return crn
+
+def model_polar(psrs, upper_limit=False, bayesephem=True):
+    """
+    Reads in list of enterprise Pulsar instance and returns a PTA
+    instantiated with model 3A from the analysis paper:
+
+    per pulsar:
+        1. fixed EFAC per backend/receiver system
+        2. fixed EQUAD per backend/receiver system
+        3. fixed ECORR per backend/receiver system
+        4. Red noise modeled as a power-law with 30 sampling frequencies
+        5. Linear timing model.
+
+    global:
+        1. GWB with HD correlations modeled with user defined PSD with
+        30 sampling frequencies. Available PSDs are
+        ['powerlaw', 'turnover' 'spectrum']
+        2. Optional physical ephemeris modeling.
+
+    :param psd:
+        PSD to use for common red noise signal. Available options
+        are ['powerlaw', 'turnover' 'spectrum'] 'powerlaw' is default
+        value.
+    :param gamma_common:
+        Fixed common red process spectral index value. By default we
+        vary the spectral index over the range [0, 7].
+    :param upper_limit:
+        Perform upper limit on common red noise amplitude. By default
+        this is set to False. Note that when perfoming upper limits it
+        is recommended that the spectral index also be fixed to a specific
+        value.
+    :param bayesephem:
+        Include BayesEphem model. Set to False by default
+    """
+
+    amp_prior = 'uniform' if upper_limit else 'log-uniform'
+
+    # find the maximum time span to set GW frequency sampling
+    tmin = [p.toas.min() for p in psrs]
+    tmax = [p.toas.max() for p in psrs]
+    Tspan = np.max(tmax) - np.min(tmin)
+
+    # white noise
+    s = white_noise_block(vary=False)
+
+    # red noise
+    s += red_noise_block(prior=amp_prior, Tspan=Tspan)
+
+    # common red noise block
+    s += common_red_noise_block(psd='inflation', prior=amp_prior, Tspan=Tspan, orf='TT')
+    s += common_red_noise_block(psd='inflation', prior=amp_prior, Tspan=Tspan, orf='ST')    
+    s += common_red_noise_block(psd='inflation', prior=amp_prior, Tspan=Tspan, orf='VL')
+    s += common_red_noise_block(psd='inflation', prior=amp_prior, Tspan=Tspan, orf='SL')
+
+    # ephemeris model
+    if bayesephem:
+        s += deterministic_signals.PhysicalEphemerisSignal(use_epoch_toas=True)
+
+    # timing model
+    s += gp_signals.TimingModel()
+
+    # set up PTA
+    pta = signal_base.PTA([s(psr) for psr in psrs])
+
+    # set white noise parameters
+    noisedict = get_noise_dict(psrlist=[p.name for p in psrs])
+    pta.set_default_params(noisedict)
+
+    return pta
---
+++ code/utils.py
@@ -0,0 +1,419 @@
+from __future__ import (absolute_import, division,
+                        print_function)
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+import json
+import os
+import hashlib
+
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+from enterprise.pulsar import Pulsar
+from PTMCMCSampler.PTMCMCSampler import PTSampler as ptmcmc
+
+CACHEDIR = '.data_cache/'
+os.system('mkdir -p {}'.format(CACHEDIR))
+
+def get_pulsars_ipta(psrlist=None, ephem='DE436', use_cache=True):
+    """
+    Reads in list of pulsar names and ephemeris version
+    and returns list of instantiated enterprise Pulsar objects.
+
+    By default the input list is None and we use the 34 pulsars used in
+    the stochastic background analysis. The default ephemeris is that used
+    in the 11-year dataset (DE436)
+    """
+    DATADIR = "$HOME/projects/working/DR2/release/VersionB/"
+    
+    if psrlist is None:
+        psrlist = np.loadtxt('psrlist-ipta.txt', dtype=np.unicode_)
+
+    # create pulsar hash
+    psr_str = ''.join(sorted(psrlist)) + ephem
+    psr_hash = hashlib.sha1(psr_str.encode()).hexdigest()
+
+    # check for cached file
+    cached_file = CACHEDIR + psr_hash
+    if os.path.exists(cached_file) and use_cache:
+        print('Reading pulsars from cached file.\n')
+        with open(cached_file, 'rb') as fin:
+            psrs = pickle.load(fin)
+    else:
+        psrs = []
+        for pulsar in psrlist:
+            print("Add pulsar" + pulsar)
+            par = DATADIR + pulsar + "/" + pulsar + ".IPTADR2.par"
+            tim = DATADIR + pulsar + "/" + pulsar + ".IPTADR2.tim"
+            psrs.append(Pulsar(par, tim, ephem=ephem))
+            
+        print('Writing pulsars to cache.\n')
+        with open(cached_file, 'wb') as fout:
+            pickle.dump(psrs, fout)
+
+    return psrs
+
+def get_pulsars(psrlist=None, ephem='DE436', use_cache=True):
+    """
+    Reads in list of pulsar names and ephemeris version
+    and returns list of instantiated enterprise Pulsar objects.
+
+    By default the input list is None and we use the 34 pulsars used in
+    the stochastic background analysis. The default ephemeris is that used
+    in the 11-year dataset (DE436)
+    """
+    
+    DATADIR = 'nano11y_data/'
+    
+    if psrlist is None:
+        psrlist = np.loadtxt('psrlist.txt', dtype=np.unicode_)
+
+    parfiles = sorted(glob(DATADIR+'/partim/*.par'))
+    timfiles = sorted(glob(DATADIR+'/partim/*.tim'))
+
+    # create pulsar hash
+    psr_str = ''.join(sorted(psrlist)) + ephem
+    psr_hash = hashlib.sha1(psr_str.encode()).hexdigest()
+
+    # check for cached file
+    cached_file = CACHEDIR + psr_hash
+    if os.path.exists(cached_file) and use_cache:
+        print('Reading pulsars from cached file.\n')
+        with open(cached_file, 'rb') as fin:
+            psrs = pickle.load(fin)
+    else:
+        psrs = []
+        for par, tim in zip(parfiles, timfiles):
+            pname = par.split('/')[-1].split('_')[0]
+            if pname in psrlist:
+                psrs.append(Pulsar(par, tim, ephem=ephem))
+
+        print('Writing pulsars to cache.\n')
+        with open(cached_file, 'wb') as fout:
+            pickle.dump(psrs, fout)
+
+    return psrs
+
+def get_noise_dict(psrlist=None):
+    """
+    Reads in list of pulsar names and returns dictionary
+    of {parameter_name: value} for all noise parameters.
+
+    By default the input list is None and we use the 34 pulsars used in
+    the stochastic background analysis.
+    """
+    DATADIR = 'nano11y_data/'
+
+    if psrlist is None:
+        psrlist = np.loadtxt('psrlist.txt', dtype=np.unicode_)
+
+    params = {}
+    for p in psrlist:
+        with open(DATADIR+'/noisefiles/{}_noise.json'.format(p), 'r') as fin:
+            params.update(json.load(fin))
+
+    return params
+
+class JumpProposal(object):
+
+    def __init__(self, pta):
+        """Set up some custom jump proposals"""
+        self.params = pta.params
+        self.pnames = pta.param_names
+        self.npar = len(pta.params)
+        self.ndim = sum(p.size or 1 for p in pta.params)
+
+        # parameter map
+        self.pmap = {}
+        ct = 0
+        for p in pta.params:
+            size = p.size or 1
+            self.pmap[p] = slice(ct, ct+size)
+            ct += size
+
+        # parameter indices map
+        self.pimap = {}
+        for ct, p in enumerate(pta.param_names):
+            self.pimap[p] = ct
+
+        self.snames = {}
+        for sc in pta._signalcollections:
+            for signal in sc._signals:
+                self.snames[signal.signal_name] = signal.params
+
+    def draw_from_prior(self, x, iter, beta):
+        """Prior draw.
+
+        The function signature is specific to PTMCMCSampler.
+        """
+
+        q = x.copy()
+        lqxy = 0
+
+        # randomly choose parameter
+        idx = np.random.randint(0, self.npar)
+
+        # if vector parameter jump in random component
+        param = self.params[idx]
+        if param.size:
+            idx2 = np.random.randint(0, param.size)
+            q[self.pmap[param]][idx2] = param.sample()[idx2]
+
+        # scalar parameter
+        else:
+            q[idx] = param.sample()
+
+        # forward-backward jump probability
+        lqxy = param.get_logpdf(x[self.pmap[param]]) - param.get_logpdf(q[self.pmap[param]])
+
+        return q, float(lqxy)
+
+    def draw_from_gwb_prior(self, x, iter, beta):
+
+        q = x.copy()
+        lqxy = 0
+
+        signal_name = 'red noise'
+
+        # draw parameter from signal model
+        param = np.random.choice(self.snames[signal_name])
+        if param.size:
+            idx2 = np.random.randint(0, param.size)
+            q[self.pmap[param]][idx2] = param.sample()[idx2]
+
+        # scalar parameter
+        else:
+            q[self.pmap[param]] = param.sample()
+
+        # forward-backward jump probability
+        lqxy = param.get_logpdf(x[self.pmap[param]]) - param.get_logpdf(q[self.pmap[param]])
+
+        return q, float(lqxy)
+
+    def draw_from_gwb_log_uniform_distribution(self, x, iter, beta):
+
+        q = x.copy()
+        lqxy = 0
+
+        # draw parameter from signal model
+        idx = self.pnames.index('log10_A_gw')
+        q[idx] = np.random.uniform(-18, -11)
+
+        return q, 0
+
+    def draw_from_dipole_log_uniform_distribution(self, x, iter, beta):
+
+        q = x.copy()
+        lqxy = 0
+
+        # draw parameter from signal model
+        idx = self.pnames.index('log10_A_dipole')
+        q[idx] = np.random.uniform(-18, -11)
+
+        return q, 0
+
+    def draw_from_monopole_log_uniform_distribution(self, x, iter, beta):
+
+        q = x.copy()
+        lqxy = 0
+
+        # draw parameter from signal model
+        idx = self.pnames.index('log10_A_monopole')
+        q[idx] = np.random.uniform(-18, -11)
+
+        return q, 0
+
+    def draw_from_ephem_prior(self, x, iter, beta):
+
+        q = x.copy()
+        lqxy = 0
+
+        signal_name = 'phys_ephem'
+
+        # draw parameter from signal model
+        param = np.random.choice(self.snames[signal_name])
+        if param.size:
+            idx2 = np.random.randint(0, param.size)
+            q[self.pmap[param]][idx2] = param.sample()[idx2]
+
+        # scalar parameter
+        else:
+            q[self.pmap[param]] = param.sample()
+
+        # forward-backward jump probability
+        lqxy = param.get_logpdf(x[self.pmap[param]]) - param.get_logpdf(q[self.pmap[param]])
+
+        return q, float(lqxy)
+
+
+def get_global_parameters(pta):
+    """Utility function for finding global parameters."""
+    pars = []
+    for sc in pta._signalcollections:
+        pars.extend(sc.param_names)
+
+    gpars = np.unique(list(filter(lambda x: pars.count(x)>1, pars)))
+    ipars = np.array([p for p in pars if p not in gpars])
+
+    return gpars, ipars
+
+
+def get_parameter_groups(pta):
+    """Utility function to get parameter groupings for sampling."""
+    ndim = len(pta.param_names)
+    groups  = [range(0, ndim)]
+    params = pta.param_names
+
+    # get global and individual parameters
+    gpars, ipars = get_global_parameters(pta)
+    if any(gpars):
+        groups.extend([[params.index(gp) for gp in gpars]])
+
+    for sc in pta._signalcollections:
+        for signal in sc._signals:
+            ind = [params.index(p) for p in signal.param_names if p not in gpars]
+            if ind:
+                groups.extend([ind])
+
+    return groups
+
+
+def setup_sampler(pta, outdir='chains', resume=False):
+    """
+    Sets up an instance of PTMCMC sampler.
+
+    We initialize the sampler the likelihood and prior function
+    from the PTA object. We set up an initial jump covariance matrix
+    with fairly small jumps as this will be adapted as the MCMC runs.
+
+    We will setup an output directory in `outdir` that will contain
+    the chain (first n columns are the samples for the n parameters
+    and last 4 are log-posterior, log-likelihood, acceptance rate, and
+    an indicator variable for parallel tempering but it doesn't matter
+    because we aren't using parallel tempering).
+
+    We then add several custom jump proposals to the mix based on
+    whether or not certain parameters are in the model. These are
+    all either draws from the prior distribution of parameters or
+    draws from uniform distributions.
+    """
+
+    # dimension of parameter space
+    ndim = len(pta.param_names)
+
+    # initial jump covariance matrix
+    cov = np.diag(np.ones(ndim) * 0.1**2)
+
+    # parameter groupings
+    groups = get_parameter_groups(pta)
+
+    sampler = ptmcmc(ndim, pta.get_lnlikelihood, pta.get_lnprior, cov, groups=groups,
+                     outDir=outdir, resume=resume)
+    np.savetxt(outdir+'/pars.txt',
+               list(map(str, pta.param_names)), fmt='%s')
+    np.savetxt(outdir+'/priors.txt',
+               list(map(lambda x: str(x.__repr__()), pta.params)), fmt='%s')
+
+    # additional jump proposals
+    jp = JumpProposal(pta)
+
+    # always add draw from prior
+    sampler.addProposalToCycle(jp.draw_from_prior, 5)
+
+    # Ephemeris prior draw
+    if 'd_jupiter_mass' in pta.param_names:
+        print('Adding ephemeris model prior draws...\n')
+        sampler.addProposalToCycle(jp.draw_from_ephem_prior, 10)
+
+    # GWB uniform distribution draw
+    if 'log10_A_gw' in pta.param_names:
+        print('Adding GWB uniform distribution draws...\n')
+        sampler.addProposalToCycle(jp.draw_from_gwb_log_uniform_distribution, 10)
+
+    # Dipole uniform distribution draw
+    if 'log10_A_dipole' in pta.param_names:
+        print('Adding dipole uniform distribution draws...\n')
+        sampler.addProposalToCycle(jp.draw_from_dipole_log_uniform_distribution, 10)
+
+    # GWB uniform distribution draw
+    if 'log10_A_monopole' in pta.param_names:
+        print('Adding monopole uniform distribution draws...\n')
+        sampler.addProposalToCycle(jp.draw_from_monopole_log_uniform_distribution, 10)
+
+    return sampler
+
+class PostProcessing(object):
+
+    def __init__(self, chain, pars, burn_percentage=0.25):
+        burn = int(burn_percentage*chain.shape[0])
+        self.chain = chain[burn:]
+        self.pars = pars
+
+    def plot_trace(self, plot_kwargs={}):
+        ndim = len(self.pars)
+        if ndim > 1:
+            ncols = 4
+            nrows = int(np.ceil(ndim/ncols))
+        else:
+            ncols, nrows = 1,1
+
+        fig = plt.figure(figsize=(15, 2*nrows))
+        for ii in range(ndim):
+            plt.subplot(nrows, ncols, ii+1)
+            plt.plot(self.chain[:, ii], **plot_kwargs)
+            plt.title(self.pars[ii], fontsize=8)
+        plt.tight_layout()
+
+    def plot_hist(self, hist_kwargs={'bins':50, 'normed':True}):
+        ndim = len(self.pars)
+        if ndim > 1:
+            ncols = 4
+            nrows = int(np.ceil(ndim/ncols))
+        else:
+            ncols, nrows = 1,1
+
+        fig = plt.figure(figsize=(15, 2*nrows))
+        for ii in range(ndim):
+            plt.subplot(nrows, ncols, ii+1)
+            plt.hist(self.chain[:, ii], **hist_kwargs)
+            plt.title(self.pars[ii], fontsize=8)
+        plt.tight_layout()
+
+
+def bayes_fac(samples, ntol=200):
+    """
+    Computes the Savage Dickey Bayes Factor and uncertainty.
+
+    :param samples: MCMC samples of GWB (or common red noise) amplitude
+    :param ntol: Tolerance on number of samples in bin
+
+    :returns: (bayes factor, 1-sigma bayes factor uncertainty)
+    """
+
+#     logAmin = -5
+    logAmin = np.percentile(samples, q=1)
+    logAmax = 0 
+
+    prior = 1 / (logAmax - logAmin)
+    #dA = np.linspace(0.01, 0.1, 100)
+    dA = np.linspace(0.01, 0.1, ntol//2)
+    bf = []
+    bf_err = []
+    mask = [] # selecting bins with more than 200 samples
+
+    for ii,delta in enumerate(dA):
+        n = np.sum(samples <= (logAmin + delta))
+        N = len(samples)
+
+        post = n / N / delta
+
+        bf.append(prior/post)
+        bf_err.append(bf[ii]/np.sqrt(n))
+
+        if n > ntol:
+            mask.append(ii)
+
+    return np.mean(np.array(bf)[mask]), np.std(np.array(bf)[mask])
